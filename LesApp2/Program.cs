5508d32dc9668efd768d3c047c3d6c2f58abb3c4
Bogdan <40561032+BogdanPinchuk@users.noreply.github.com>
2019-08-10 22:36:15 +0300

    Доролено LesApp2

--- LesApp2/Program.cs
+++ LesApp2/Program.cs
@@ -10,7 +10,91 @@ namespace LesApp2
     {
         static void Main()
         {
+            // Join Unicode
+            Console.OutputEncoding = Encoding.Unicode;
 
+            // створення списків зі словами
+            List<string> en = new List<string>(),
+                ua = new List<string>(),
+                ru = new List<string>();
+            en.AddRange(new string[]
+            {
+                "God",
+                "Good",
+                "Sun",
+                "Boy",
+                "Girl",
+                "Rainbow",
+                "Happyness",
+                "Mother",
+                "Father",
+                "Brain",
+            });
+            ua.AddRange(new string[]
+            {
+                "Бог",
+                "Добро",
+                "Сонце",
+                "Хлопець",
+                "Дівчина",
+                "Веселка",
+                "Щастя",
+                "Мати",
+                "Батько",
+                "Мозок",
+            });
+            ru.AddRange(new string[]
+            {
+                "Бог",
+                "Благо",
+                "Солнце",
+                "Парень",
+                "Девушка",
+                "Радуга",
+                "Счастье",
+                "Мать",
+                "Отец",
+                "Мозг",
+            });
+
+            // колекція
+            var dictionary = en.Zip(ua.Zip(ru, (u, r) => new { Ua = u, Ru = r }), (e, ur) => new { En = e, ur.Ua, ur.Ru });
+
+            // Вивід даних
+            Console.WriteLine("\n\tДані словника:\n");
+
+            foreach (dynamic i in dictionary)
+            {
+                Console.WriteLine($"\tEng: {i.En}, Ukr: {i.Ua}, Rus: {i.Ru};");
+            }
+
+            // repeat
+            DoExitOrRepeat();
+        }
+
+        /// <summary>
+        /// Метод виходу або повторення методу Main()
+        /// </summary>
+        static void DoExitOrRepeat()
+        {
+            Console.WriteLine("\n\nСпробувати ще раз: [т, н]");
+            Console.Write("\t");
+            var button = Console.ReadKey(true);
+
+            if ((button.KeyChar.ToString().ToLower() == "т") ||
+                (button.KeyChar.ToString().ToLower() == "n")) // можливо забули переключити розкладку клавіатури
+            {
+                Console.Clear();
+                Main();
+                // без використання рекурсії
+                //Process.Start(Assembly.GetExecutingAssembly().Location);
+                //Environment.Exit(0);
+            }
+            else
+            {
+                // закриває консоль
+                Environment.Exit(0);
+            }
         }
     }
 }
